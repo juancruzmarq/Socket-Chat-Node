8623db81e171ea47ac3b2f16860dca337d16efe9
Juan Cruz <marquezjuanchy@hotmail.com>
2022-01-07 19:22:24 -0300

    1st commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,60 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+var nombre = params.get('nombre')
+var sala = params.get('sala')
+
+if(!nombre|| !sala){
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+}
+ 
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp){
+        console.log('Usuarios conectados', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('enviarMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+
+
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
+
+//Escuchar cambios de usuarios
+//cuando un usuario entra o sale
+
+socket.on('listaPersona', function(personas){
+    console.log(personas);
+})
+
+//mensajes privados
+
+socket.on('mensajePrivado', function(mensaje){
+    console.log('Mensaje Privado', mensaje)
+})
